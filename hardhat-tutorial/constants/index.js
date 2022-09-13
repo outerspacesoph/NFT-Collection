947f7005e7e17b0ea745fddfd5da085da01ba44d
outerspacesoph <sophlamo97@gmail.com>
2022-09-13 13:30:44 -0600

    part1

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+
+const WHITELIST_CONTRACT_ADDRESS = "0x885A14EBb1d7a79A1A810e268aD95731fF6cA7d0";
+
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
